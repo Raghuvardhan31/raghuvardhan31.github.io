05badd5946a7be8f81656b42c134c5c8189c4b26
Raghuvardhan <bonjuriraghuvardhan@gmail.com>
2025-12-02 16:41:08 +0530

    temp commmit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/temp_chess/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
